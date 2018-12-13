c8d8109ed9334cadbdb064f631fda42342b75bf9
AlshimaaRifaat <alshimaa.rifaat71@gmail.com>
2018-12-13 15:50:54 +0200

    search done+btn show details

---
+++ app/src/main/java/com/example/shosho/coupmix/view/OnClickDetailsCategoryItemView.java
@@ -0,0 +1,8 @@
+package com.example.shosho.coupmix.view;
+
+import com.example.shosho.coupmix.model.CategoryItemDetails;
+import com.example.shosho.coupmix.model.FeatureProductDetails;
+
+public interface OnClickDetailsCategoryItemView {
+    void showOnClickDetailsCategoryItemResult(CategoryItemDetails categoryItemDetails);
+}
