4c43e2b87b5d9649212d567e0aa0fe15c8fde00a
AlshimaaRifaat <alshimaa.rifaat71@gmail.com>
2019-01-14 15:41:05 +0200

    all app done+gallery design+sponsor design

---
+++ app/src/main/java/com/example/shosho/coupmix/fragment/SponsorFragment.java
@@ -0,0 +1,61 @@
+package com.example.shosho.coupmix.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shosho.coupmix.R;
+import com.example.shosho.coupmix.activity.NavigationActivity;
+
+import static com.example.shosho.coupmix.activity.NavigationActivity.toolbar;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SponsorFragment extends Fragment {
+
+
+    public SponsorFragment() {
+        // Required empty public constructor
+    }
+
+View view;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view= inflater.inflate( R.layout.fragment_sponsor, container, false );
+        init();
+        NavigationActivity.toggle = new ActionBarDrawerToggle(
+                getActivity(), NavigationActivity.drawer, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        NavigationActivity.drawer.addDrawerListener(NavigationActivity.toggle);
+        NavigationActivity.toggle.syncState();
+
+        NavigationActivity.toggle.setDrawerIndicatorEnabled(false);
+        toolbar.setNavigationIcon(R.drawable. icon_menu);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                if (NavigationActivity.drawer.isDrawerOpen(GravityCompat.START)) {
+                    NavigationActivity.drawer.closeDrawer(GravityCompat.START);
+                } else {
+                    NavigationActivity.drawer.openDrawer(GravityCompat.START);
+                }
+            }
+        });
+        return view;
+    }
+
+    private void init() {
+        toolbar=view.findViewById( R.id.sponsor_toolbar );
+    }
+
+}
