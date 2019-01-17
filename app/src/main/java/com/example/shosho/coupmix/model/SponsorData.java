c0748157cc74a5cb1c89faeb57ebf5c01b411786
AlshimaaRifaat <alshimaa.rifaat71@gmail.com>
2019-01-17 13:52:36 +0200

    all app done except add icon for video

---
+++ app/src/main/java/com/example/shosho/coupmix/model/SponsorData.java
@@ -0,0 +1,63 @@
+package com.example.shosho.coupmix.model;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class SponsorData {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("image")
+    @Expose
+    private String image;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("address")
+    @Expose
+    private String address;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
---
+++ app/src/main/java/com/example/shosho/coupmix/model/SponsorResponse.java
@@ -0,0 +1,43 @@
+package com.example.shosho.coupmix.model;
+
+import java.util.List;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class SponsorResponse {
+
+    @SerializedName("data")
+    @Expose
+    private List<SponsorData> data = null;
+    @SerializedName("status")
+    @Expose
+    private Boolean status;
+    @SerializedName("error")
+    @Expose
+    private String error;
+
+    public List<SponsorData> getData() {
+        return data;
+    }
+
+    public void setData(List<SponsorData> data) {
+        this.data = data;
+    }
+
+    public Boolean getStatus() {
+        return status;
+    }
+
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+}
